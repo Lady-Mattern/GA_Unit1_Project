49d9ca731960985cf0695497573b68c90e4b2377
Leah Mattern <lady-mattern@Leahs-MBP.fios-router.home>
2020-04-01 17:58:13 -0400

    Pre-class Project commit

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ console.log("I'm alive!");
 
 const pianoArr = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 
+const mozartTurn = () => {
+
+}
+
+const playerTurn = () => {
+
+}
+
 $(() => {
-    
+
+    const startGame = () => {
+        alert('Wanna make music with Mozart?');
+    } 
 
 
 })
